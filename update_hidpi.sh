e06f70cc61f574122333463deaf0aef197462bb3
HexaTom <26824415+H3xaT0m@users.noreply.github.com>
2021-07-29 10:13:11 +0000

    fix update_hidpi.sh

--- update_hidpi.sh
+++ update_hidpi.sh
@@ -7,4 +7,4 @@ set -e
 sudo /usr/bin/pacman -Syq --noconfirm ansible git
 
 # run ansible
-/usr/bin/ansible-pull -U https://github.com/H3xaT0m/ansible-workstation.git --extra-vers "ansible_user=$(whoami)" --extra-vars "hidpi=true" main.yml
+/usr/bin/ansible-pull -U https://github.com/H3xaT0m/ansible-workstation.git --extra-vars "ansible_user=$(whoami)" --extra-vars "hidpi=true" main.yml
